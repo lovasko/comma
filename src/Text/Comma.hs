b4c585bccbbc4adf08c822bfebbeec11aa6e1708
Daniel Lovasko <daniel.lovasko@gmail.com>
2017-02-07 01:24:07 +0000

    removed support for \r

--- src/Text/Comma.hs
+++ src/Text/Comma.hs
@@ -26,7 +26,7 @@ import qualified Data.Text as T
 field :: A.Parser T.Text -- ^ parser
 field = fmap T.concat quoted <|> normal A.<?> "field"
   where
-    normal  = A.takeWhile (A.notInClass "\n\r,\"")     A.<?> "normal field"
+    normal  = A.takeWhile (A.notInClass "\n,\"")       A.<?> "normal field"
     quoted  = A.char '"' *> many between <* A.char '"' A.<?> "quoted field"
     between = A.takeWhile1 (/= '"') <|> (A.string "\"\"" *> pure "\"")
 
@@ -35,8 +35,8 @@ comma :: T.Text                   -- ^ CSV text
       -> Either String [[T.Text]] -- ^ error | table
 comma text = A.parseOnly table fixEnd
   where
-    table  = A.sepBy1 record A.endOfLine A.<?> "table"
-    record = A.sepBy1 field (A.char ',') A.<?> "record"
+    table  = A.sepBy1 record (A.char '\n') A.<?> "table"
+    record = A.sepBy1 field (A.char ',')   A.<?> "record"
     fixEnd = maybe text id (T.stripSuffix "\n" text)
 
 -- | Render a table of texts into a valid CSV output.
@@ -44,7 +44,7 @@ uncomma :: [[T.Text]] -- ^ table
         -> T.Text     -- ^ CSV text
 uncomma = T.unlines . map (T.concat . intersperse "," . map conv)
   where
-    isQuoted  = T.any (`elem` ['"', '\n', '\r', ','])
+    isQuoted  = T.any (`elem` ['"', '\n', ','])
     enquote x = T.concat ["\"", x, "\""]
     conv f
       | isQuoted f = enquote (T.replace "\"" "\"\"" f)
